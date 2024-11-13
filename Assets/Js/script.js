b2b80d0734bfac649fc5436220ca7416de350025
Dan131195 <daniele.franceschini1395@gmail.com>
2024-11-13 16:41:15 +0100

    Esercizo

---
+++ Assets/Js/script.js
@@ -0,0 +1,41 @@
+const divList = document.getElementById("list");
+const text = document.getElementById("text");
+const btnText = document.getElementById("btnText");
+const taskList = document.getElementById("taskList");
+const list = [];
+
+btnText.addEventListener("click", function (e) {
+  e.preventDefault();
+  addArray();
+  addList();
+  list.reset();
+});
+
+function addArray() {
+  list.push(text.value);
+}
+
+function addList() {
+  taskList.innerHTML = "";
+  for (let i = 0; i < list.length; i++) {
+    let newItem = document.createElement("li");
+    newItem.setAttribute("onclick", `done(${i})`);
+    newItem.innerText = list[i];
+    let button = document.createElement("button");
+    button.setAttribute("type", "button");
+    button.setAttribute("id", "btnDelete");
+    button.setAttribute("onclick", `deleteItem(${i})`);
+    button.innerHTML = '<i class="fa-light fa-trash-can"></i>';
+    newItem.appendChild(button);
+    taskList.appendChild(newItem);
+  }
+}
+
+function deleteItem(index) {
+  list.splice(index, 1);
+  addList();
+}
+
+function done() {
+  list.style.textDecoration = "line-through";
+}
